2cd8f89ff592cebd1f5779e176b5624fefe999ff
Felipe Quintero <ffquint@gmail.com>
2015-12-29 13:01:39 -0800

    Created migration to add name and description columns to List model. Added missing attributes to List and Item serializer.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151229182715) do
+ActiveRecord::Schema.define(version: 20151229205036) do
 
   create_table "items", force: :cascade do |t|
     t.string   "name"
@@ -23,9 +23,14 @@ ActiveRecord::Schema.define(version: 20151229182715) do
   add_index "items", ["user_id"], name: "index_items_on_user_id"
 
   create_table "lists", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.string   "name"
+    t.text     "description"
   end
+
+  add_index "lists", ["description"], name: "index_lists_on_description"
+  add_index "lists", ["name"], name: "index_lists_on_name"
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
